773d3382d857e84c1262b83df1a6fbb22af23873
Joao Pedro <joopedrof.silva@gmail.com>
2022-07-12 02:44:26 -0300

    Final version

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <tuple>
-
 #include "genMaxArea.h"
 
 using namespace std;
+
+//Funcao para ordenar a quadrupla em ordem descendente, e utilizada no trecho final do main.
+bool desc(const tuple<int, int, int, bool>& first,
+	const tuple<int, int, int, bool>& second)
+{
+	return (get<0>(first) > get<0>(second));
+}
+
+
 int main(void) {
 
 	int rows, collums;
@@ -50,7 +58,9 @@ int main(void) {
 	
 	//Invoca a funcao que gera a lista das maiores areas por dimensao
 	vector<int> result = genMaxArea(matrix);
-
+	/*for (int i = 0; i < result.size(); i++) {
+		cout << "RESULT = " << i << " X " << result[i] << endl;
+	}*/
 
 	int n;
 	cin >> n;
@@ -69,48 +79,63 @@ int main(void) {
 		int area = higher * lower;
 		tables[i] = make_tuple(area, higher, lower, flag);
 	}
-	//Ordena as mesas por area, de forma decrescente
-	sort(tables.begin(), tables.end(), greater<tuple<int, int, int, bool>>());
 
+
+	//Ordena as mesas por area, de forma decrescente
+	sort(tables.begin(), tables.end(), desc);
+	for (int i = 0; i < tables.size(); i++) {
+		cout << get<0>(tables[i]) << " " << get<1>(tables[i]) << " " << get<2>(tables[i]) << "\n";
+	}
+
+
+	bool already_chosen = 0;
 	pair<int, int>chosen_table;
 	for (int i = 0; i < tables.size(); i++) {
-		bool already_chosen = 0;
+		bool end = 0;
 		int bigger_side = get<1>(tables[i]);
 		int smaller_side = get<2>(tables[i]);
+		//Caso a mesa tenha algum tamanho maior que alguma parece da sala, ela e negada
+		if (bigger_side > result.size()) {
+			continue;
+		}
+		//Testa se a mesa e viavel para alguma solucao encontrada
+		for (int j = bigger_side; j < result.size(); j++) {
+			if (result[j] >= smaller_side) {
+				//Caso a mesa tenha sido rotacionada, inverte os lados
+				if (get<3>(tables[i])) {
+					int temp = get<1>(tables[i]);
+					get<1>(tables[i]) = get<2>(tables[i]);
+					get<2>(tables[i]) = temp;
+					get<3>(tables[i]) = 0;
+				}
 
-		if (result[bigger_side] > smaller_side) {
-			//Caso a mesa tenha sido rotacionada, inverte os lados
-			if (get<3>(tables[i])) {
-				int temp = get<1>(tables[i]);
-				get<1>(tables[i]) = get<2>(tables[i]);
-				get<2>(tables[i]) = temp;
-				get<3>(tables[i]) = 0;
-			}
-
-			bool end = 0;
-			if (already_chosen) {
-				//Caso ja tenha sido escolhida alguma mesa, compara com a resposta da proxima mesa possivel
-				if (get<0>(tables[i]) == get<0>(tables[i - 1])) {
-					//Caso elas tenham a mesma area, compara o comprimento de cada uma das mesas
-					if (get<2>(tables[i]) > get<2>(tables[i - 1])) {
-						chosen_table.first = get<1>(tables[i]);
-						chosen_table.second = get<2>(tables[i]);
+				if (already_chosen) {
+					//Caso ja tenha sido escolhida alguma mesa, compara com a resposta da proxima mesa possivel
+					if (get<0>(tables[i]) == get<0>(tables[i - 1])) {
+						//Caso elas tenham a mesma area, compara o comprimento de cada uma das mesas
+						if (get<2>(tables[i]) > get<2>(tables[i - 1])) {
+							chosen_table.first = get<1>(tables[i]);
+							chosen_table.second = get<2>(tables[i]);
+						}
+					}
+					else {
+						//Caso eles nao tenham a mesma area, quer dizer que a atual mesa escolhida é a otima
+						end = 1;
 					}
 				}
 				else {
-					//Caso eles nao tenham a mesma area, quer dizer que a atual mesa escolhida é a otima
-					end = 1;
+					already_chosen = 1;
+					chosen_table.first = get<1>(tables[i]);
+					chosen_table.second = get<2>(tables[i]);
+					break;
+				}
+				if (end == 1) {
+					break;
 				}
 			}
-			else {
-				already_chosen = 1;
-				chosen_table.first = get<1>(tables[i]);
-				chosen_table.second = get<2>(tables[i]);
-			}
-			if (end == 1) {
-				break;
-			}
-			
+		}
+		if (end == 1) {
+			break;
 		}
 	}
 	cout << chosen_table.first << " " << chosen_table.second;
